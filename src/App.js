ac77b9f43f54319b6d33a039221c0bba396c7742
sneha-verma46 <sneha.28october@gmail.com>
2025-04-23 17:50:17 +0530

    Ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import Alert from './components/Alert';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({ msg: message, type: type });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "white";
+      showAlert("Dark Mode has been Enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+
+      // setInterval(() => { document.title = "You have been traced" }, 1000);
+      // setInterval(() => { document.title = "Yeahhh" }, 2000);
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      showAlert("Light Mode has been Enabled", "success");
+      document.title = "TextUtils - Light Mode";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar
+        title="TextConvertor"
+        Link="Links"
+        Home="About"
+        Dropdown="Dropdown1"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
+      <div className="container mb-3">
+        <Routes>
+          <Route
+            path="/"
+            element={<TextForm showAlert={showAlert} TextArea="Enter Your Text Here To Analyze" />}
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
